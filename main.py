0098fda860dbcfc49690611cd77c24f5f1b110e6
xmlregeneratoroff <kinchiktube@gmail.com>
2025-11-18 01:20:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ MAX_WORKERS = 5
 def setup_logging():
     """Настройка логирования"""
     logger.remove()
-    logger.add(sys.stdout, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>")
+    logger.add(sys.stdout, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>", level="INFO")
     logger.add("feed_processor.log", rotation="10 MB", retention="1 week", level="DEBUG")
 
 
